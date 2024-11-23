31b7a8cd4b55c72b0531036428b0844688410fe9
rovshen <rovsen.dev@gmail.com>
2024-11-23 13:30:42 +0400

    Коррекция изображений в ShopTheLookSlider

--- src/pages/Home/ShopTheLookSlider/ShopTheLookSliderData.js
+++ src/pages/Home/ShopTheLookSlider/ShopTheLookSliderData.js
@@ -1,57 +1,16 @@
-// import image1 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_1.webp';
-// import image2 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_2.webp';
-// import image3 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_3.webp';
-// import image4 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_4.webp';
-// import image5 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_5.webp';
-// import image6 from '../../../assets/img/PerfectMatch_img/PerfectMatch_img_6.webp';
-
-// const ShopTheLookSliderData = [
-//   {
-//     id: 1,
-//     imageUrl: image1,
-//   },
-//   {
-//     id: 2,
-//     imageUrl: image2,
-//   },
-//   {
-//     id: 3,
-//     imageUrl: image3,
-//   },
-//   {
-//     id: 4,
-//     imageUrl: image4,
-//   },
-//   {
-//     id: 5,
-//     imageUrl: image5,
-//   },
-//   {
-//     id: 6,
-//     imageUrl: image6,
-//   },
-//   // Добавляйте другие элементы по мере необходимости
-// ];
-
-// export default ShopTheLookSliderData;
+import image1 from "../../../assets/img/ShopTheLookSlider_img/Image_block_1/Img_block_1_500x700x1.webp";
+import image2 from "../../../assets/img/ShopTheLookSlider_img/Image_block_2/Img_block_2_500x700x1.png";
+import image3 from "../../../assets/img/ShopTheLookSlider_img/Image_block_3/Img_block_3_500x700x1.png";
+import image4 from "../../../assets/img/ShopTheLookSlider_img/Image_block_4/Img_block_4_500x700_1.png";
+import image5 from "../../../assets/img/ShopTheLookSlider_img/Image_block_5/Img_block_5_500x700x1.png";
+import image6 from "../../../assets/img/ShopTheLookSlider_img/Image_block_6/Img_block_6_500x700x1.png";
+import image7 from "../../../assets/img/ShopTheLookSlider_img/Image_block_7/Img_block_7_500x700x1.jpg";
+import image8 from "../../../assets/img/ShopTheLookSlider_img/Image_block_8/Img_block_8_500x700_1.jpg";
+import image9 from "../../../assets/img/ShopTheLookSlider_img/Image_block_9/Img_block_9_500x700_1.jpg";
 
 
 
 
-
-
-
-
-
-
-
-
-import image1 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_1.webp";
-import image2 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_2.webp";
-import image3 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_3.webp";
-import image4 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_4.webp";
-import image5 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_5.webp";
-import image6 from "../../../assets/img/PerfectMatch_img/PerfectMatch_img_6.webp";
 
 const ShopTheLookSliderData = [
   {
@@ -95,6 +54,27 @@ const ShopTheLookSliderData = [
     title: "Куртка Ветровка",
     size: "L",
     price: "5 499 ₽",
+  },
+  {
+    id: 7,
+    imageUrl: image7,
+    title: "Куртка krasnaya",
+    size: "Lg",
+    price: "5 49999 ₽",
+  },
+  {
+    id: 8,
+    imageUrl: image8,
+    title: "Uтка krasnaya",
+    size: "Hg",
+    price: "1 49999 ₽",
+  },
+  {
+    id: 9,
+    imageUrl: image9,
+    title: "Uтка krasnaya",
+    size: "Hg",
+    price: "1 49999 ₽",
   },
 ];
 
